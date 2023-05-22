06df1e1eea902eb5e20de247d00937aa4b04eecc
Giglio59 <antoinegiglioo@gmail.com>
2023-05-22 14:06:20 +0200

    finition

---
+++ Index.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyASeKywBbub0xx5vDWOS-pwl7y0bpd1eXg",
+    authDomain: "lecerveaugeek.firebaseapp.com",
+    databaseURL: "https://lecerveaugeek-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "lecerveaugeek",
+    storageBucket: "lecerveaugeek.appspot.com",
+    messagingSenderId: "950815627270",
+    appId: "1:950815627270:web:88312d734aecb23d36cb87",
+    measurementId: "G-XHK2HPXQGY"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
